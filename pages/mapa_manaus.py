b66cc28cb29d34c796d3bdb87ce9034a0612fd96
Cesar Augusto <108474749+czaraugustocoder@users.noreply.github.com>
2025-09-04 15:19:00 -0500

    Update mapa_manaus.py
    tamnaho da fonte

--- pages/mapa_manaus.py
+++ pages/mapa_manaus.py
@@ -153,7 +153,7 @@ style_function=lambda feature: {
         "background-color: white; "
         "color: #333333; "
         "font-family: Arial; "
-        "font-size: 22px; "
+        "font-size: 12px; "
         "padding: 8px;"
     ))
 ).add_to(m)
@@ -163,6 +163,7 @@ folium.LayerControl().add_to(m)
 
 
 st_folium(m, width=1000, returned_objects=[])
+
 
 
 
